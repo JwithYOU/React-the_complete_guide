f5f1a7af2fbda138fd6bb3a7b43a9bc7e7ee4444
YOU <khy960101@gmail.com>
2022-12-19 19:14:07 +0900

    pages 하위 컴포넌트 생성

--- 07-new-starting-project/src/App.js
+++ 07-new-starting-project/src/App.js
@@ -1,5 +1,23 @@
+import { Router } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetUps";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router path="/">
+        <AllMeetupsPage />
+      </Router>
+      <Router path="/new-meet-up">
+        <NewMeetupPage />
+      </Router>
+      <Router path="/favorites">
+        <FavoritesPage />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
